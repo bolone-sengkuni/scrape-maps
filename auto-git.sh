ba36f16770182cd62bdcc553f6c963c960948292
kulo-sinten <toyibnganjuk@gmail.com>
2023-04-12 00:35:24 +0800

    M

--- auto-git.sh
+++ auto-git.sh
@@ -2,12 +2,12 @@ c=1
 while true; do
 echo -e "\n==============="
 echo -e "GIT PUSH: $c"
-echo -e "===============\n"
+echo -e "==============="
 git add .
 git commit -m "M"
 git branch -M main
 git push -u origin main
 
 c=$((c+1))
-sleep 8
+sleep 5
 done
